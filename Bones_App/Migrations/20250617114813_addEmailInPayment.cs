0890eab6b7436648869efd6d5be5ba164603604e
AhmedSalah106 <ahmedsalah1062003@gmail.com>
2025-06-17 17:15:56 +0300

    my final project

---
+++ Bones_App/Migrations/20250617114813_addEmailInPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bones_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class addEmailInPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "PaymentTransactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "PaymentTransactions");
+        }
+    }
+}
